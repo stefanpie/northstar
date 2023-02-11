088d2c03a0e2de5e829df851413e5a957306d080
Stefan Abi-Karam <stefanabikaram@gatech.edu>
2023-02-10 21:01:10 -0500

    work

---
+++ northstar-frontend/src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="top-wrapper">
+    <header>
+      <h1>Northstar</h1>
+      <img src="" alt="" />
+    </header>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+
+    <RouterView class="main-view" />
+  </div>
+</template>
+<style>
+.top-wrapper {
+  display: grid;
+  grid-template-rows: 4rem 1fr;
+  grid-template-columns: 12em 1fr;
+  width: 100%;
+  height: 100%;
+}
+
+header {
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: auto auto 1fr auto;
+  grid-template-areas: 'title logo spacer';
+  grid-row: 1 / 2;
+  grid-column: 1 / 3;
+  border-bottom: var(--bw) var(--bs) var(--ct-border);
+  background-color: var(--color-color-secondary);
+  padding: 0.1rem;
+  padding-right: 1rem;
+  padding-left: 1rem;
+}
+
+header > h1 {
+  grid-area: title;
+  margin: auto;
+  padding: 0;
+}
+
+header .spacer {
+  grid-area: spacer;
+}
+
+nav {
+  display: flex;
+  grid-row: 2 / 3;
+  grid-column: 1 / 2;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border-right: var(--bw) var(--bs) var(--ct-border);
+  background-color: var(--color-color-secondary);
+  padding-top: 1rem;
+  padding-right: 1rem;
+  padding-left: 1rem;
+}
+nav > a {
+  transition: background-color 0.05s ease-in-out;
+  margin-bottom: 0.5rem;
+  border: var(--bw) var(--bs) var(--ct-border);
+  background-color: var(--ct-primary);
+  padding: 0.2rem 0.5rem;
+  width: 100%;
+  font-size: 1.2rem;
+  user-select: none;
+  text-decoration: none;
+}
+nav > a:hover,
+nav > a.router-link-exact-active {
+  border-color: var(--ct-border);
+  background-color: var(--ct-secondary);
+}
+
+.main-view {
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+  background-color: var(--ct-primary);
+  padding: 2rem;
+}
+</style>
---
+++ northstar-frontend/src/components/ColorTestGrid.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="color-test-grid">
+    <!-- 
+    --ct-primary: var(--color-blue-500);
+    --ct-secondary: #6c757d;
+    --ct-success: #28a745;
+    --ct-info: #17a2b8;
+    --ct-warning: #ffc107;
+    --ct-danger: #dc3545;
+    --ct-light: #f8f9fa;
+    --ct-dark: #343a40;
+    --ct-white: #fff;
+    --ct-black: #000;
+    -->
+    <div class="color-test-grid-primary"></div>
+    <div class="color-test-grid-secondary"></div>
+    <div class="color-test-grid-border"></div>
+    <div class="color-test-grid-success"></div>
+    <div class="color-test-grid-info"></div>
+    <div class="color-test-grid-warning"></div>
+    <div class="color-test-grid-danger"></div>
+    <div class="color-test-grid-light"></div>
+    <div class="color-test-grid-dark"></div>
+    <div class="color-test-grid-white"></div>
+    <div class="color-test-grid-black"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.color-test-grid {
+  grid-gap: 1rem;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(5, 1fr);
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+}
+
+.color-test-grid-primary {
+  background-color: var(--ct-primary);
+}
+
+.color-test-grid-secondary {
+  background-color: var(--ct-secondary);
+}
+
+.color-test-grid-border {
+  background-color: var(--ct-border);
+}
+
+.color-test-grid-success {
+  background-color: var(--ct-success);
+}
+
+.color-test-grid-info {
+  background-color: var(--ct-info);
+}
+
+.color-test-grid-warning {
+  background-color: var(--ct-warning);
+}
+
+.color-test-grid-danger {
+  background-color: var(--ct-danger);
+}
+
+.color-test-grid-light {
+  background-color: var(--ct-light);
+}
+
+.color-test-grid-dark {
+  background-color: var(--ct-dark);
+}
+
+.color-test-grid-white {
+  background-color: var(--ct-white);
+}
+
+.color-test-grid-black {
+  background-color: var(--ct-black);
+}
+</style>
